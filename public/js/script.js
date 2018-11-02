0e8368bbd4c29cddc5112b2373cf74f755855750
Daniel Pereira <danielpaulpereira@protonmail.com>
2018-11-02 16:14:16 -0400

    Initial changes to script

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,18 @@
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+const sixteen = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false];
+let kicks = sixteen;
+let snares = sixteen;
+let hiHats = sixteen;
+let rideCymbals = sixteen;
 
+const toggleDrum = (arrayName, indexNum) => {
+  arrayName[indexNum] = !arrayName[indexNum];
+};
+
+const clear = arrayName => arrayName = sixteen;
+
+const invert = (arrayName) => {
+  return arrayName = arrayName.map(function(index) {
+    return !index;
+  });
+};
